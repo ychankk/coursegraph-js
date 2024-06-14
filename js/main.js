5d3596aa52352cc6888d971147f165683973d4b0
Junhyung Park <62749830+vegahouse102@users.noreply.github.com>
2024-06-14 21:30:40 +0900

    Update main.js
    show.html 안보이는 오류 수정

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // 초기 설정
+  fetch('/data/courses.json')// 상대경로를 절대경로로 변경
+    // 파일을 가져오면 json 파일 형식으로 변환함
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(courses => {
+      // json 형식의 데이터가 전달되면 그림을 그림
+      // 기존의 코드 문단을 별도의 함수로 분리
+      drawSVGGraph(courses);
+    })
+    .catch(error => {
+      console.error('Error loading JSON:', error);
+      alert('Failed to load the JSON file. Please check the file path and try again.');
+    });
 });
 
 function drawSVGGraph(courses) {
